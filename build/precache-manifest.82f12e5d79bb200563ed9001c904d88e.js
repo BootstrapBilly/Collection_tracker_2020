46d9e8929725effc5f53b8b20923c2188378117b
Billy <billyjcatchpole@gmail.com>
2020-04-15 16:09:42 +0100

    Backend integration

---
+++ build/precache-manifest.82f12e5d79bb200563ed9001c904d88e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1eac58845bb4b6859938e71cceefa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162c19393dc9ac9f9c23",
+    "url": "/static/css/main.12a97e21.chunk.css"
+  },
+  {
+    "revision": "04c9124dc0d1b7f820f8",
+    "url": "/static/js/2.68f92c22.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.68f92c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162c19393dc9ac9f9c23",
+    "url": "/static/js/main.5200e3c1.chunk.js"
+  },
+  {
+    "revision": "0cf08e69e238fe250078",
+    "url": "/static/js/runtime-main.310ae76b.js"
+  },
+  {
+    "revision": "0eccf270c2657c0686df48b8caf66c2b",
+    "url": "/static/media/add-book-active.0eccf270.svg"
+  },
+  {
+    "revision": "f4eabc9baef19cd1b723466318bd8909",
+    "url": "/static/media/add-book.f4eabc9b.svg"
+  },
+  {
+    "revision": "940f7344da59afc184172b87cd5d2d47",
+    "url": "/static/media/devices.940f7344.svg"
+  },
+  {
+    "revision": "f5130d5121dc0fc61cc7edc6ddc24308",
+    "url": "/static/media/features.f5130d51.svg"
+  },
+  {
+    "revision": "bd4e2f7c64c094eda9a4d9ba32bf9335",
+    "url": "/static/media/home-active.bd4e2f7c.svg"
+  },
+  {
+    "revision": "165c53c544407d33be88da353b8f50e8",
+    "url": "/static/media/home.165c53c5.svg"
+  },
+  {
+    "revision": "f54ed9bea87befaa033c82752b6457b2",
+    "url": "/static/media/rotate.f54ed9be.svg"
+  },
+  {
+    "revision": "05a8d5cfc8c1d66b1b147ed59dcc16ba",
+    "url": "/static/media/search-active.05a8d5cf.svg"
+  },
+  {
+    "revision": "8fda69e68b45a3a973505975ad047587",
+    "url": "/static/media/search.8fda69e6.svg"
+  },
+  {
+    "revision": "d8adc37e081e516a9343b4fb91265fd1",
+    "url": "/static/media/worth-it-active.d8adc37e.svg"
+  },
+  {
+    "revision": "983466dd7f37d817657b9d69065ae538",
+    "url": "/static/media/worth-it.983466dd.svg"
+  }
+]);
--- src/Util/Colours.js
+++
@@ -1,3 +0,0 @@
-const colours = {red:"#DC143C", orange:"#ffa500", green:"#66CD00", blue:"#6495ED", middle_blue:"#4D86EB", dark_blue:"#3676e8", grey:"#778899", dark_grey: "#708090"};
-
-export default colours
