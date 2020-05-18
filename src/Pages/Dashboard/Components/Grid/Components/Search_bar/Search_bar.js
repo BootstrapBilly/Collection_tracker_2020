97f7e31f925f19e5233f77b11757630cf23569c6
Billy <billyjcatchpole@gmail.com>
2020-05-18 09:52:02 +0100

    Added a search box to the grid which instantly filters all books and only shows the search string

---
+++ src/Pages/Dashboard/Components/Grid/Components/Search_bar/Search_bar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import classes from "./Search_bar.module.css"
+import colours from '../../../../../../Util/Colours'
+
+export const Search_bar = props => {
+
+    return (
+
+        <div className={classes.container}>
+            
+            <input type="text" className={classes.input} style={{color:colours.dark_blue}} maxLength="4" onChange={props.handle_filter} value={props.value}/>
+
+        </div>
+
+    )
+
+}
+
+export default Search_bar
--- src/Pages/Dashboard/Components/Grid/Grid.js
+++ src/Pages/Dashboard/Components/Grid/Grid.js
@@ -1,12 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 //css 
 import classes from "./Grid.module.css"
 
 //components
 import Cell from "./Components/Cell/Cell"
+import SearchBar from "./Components/Search_bar/Search_bar"
 
 export const Grid = props => {
+
+    const [cells, set_cells] = useState([])
+    const [existing_books, set_existing_books] = useState([])
 
     const generate_cells = () => {
 
@@ -14,10 +18,10 @@ export const Grid = props => {
 
         const all_years = []
 
-        while(count <= 2020){
+        while (count <= 2020) {
 
             all_years.push(count)
-            count ++
+            count++
 
         }
 
@@ -27,33 +31,75 @@ export const Grid = props => {
     const populate_and_sort_books = () => {
 
         const books = []
-    
+
         props.books && props.books.forEach(book => books.push(book.year))
 
         return books.sort((a, b) => a < b && -1) //sort the books by condition (poor, fair ,mint)
 
     }
 
-    const [existing_books, set_existing_books] = useState([])
+    const filter_cells = () => {
+
+        let count = 1955;
+
+        const filtered_years = []
+
+        while (count <= 2020){
+
+            if(count.toString().includes(filter_string)){
+
+                filtered_years.push(count)
+
+            }
+
+            count ++
+        }
+
+        return filtered_years
+    }
+
+    
 
     useEffect(() => {
-        
-        if(props.books) set_existing_books(populate_and_sort_books())
+
+        if (props.books) set_existing_books(populate_and_sort_books())
 
     }, [props.books])
 
-   const cells = generate_cells()
+
+
+    const [filter_string, set_filter_string] = useState(null)
+
+    console.log(filter_string)
+
+    useEffect(()=> {
+
+        if(filter_string){
+
+            set_cells(filter_cells())
+
+        }
+
+    }, [filter_string])
+
+    useEffect(()=> {
+
+        set_cells(generate_cells())
+
+    },[])
 
     return (
 
         <div className={classes.container}>
 
-        {cells.map(year => {
+            <SearchBar handle_filter={e => set_filter_string(e.target.value)} value={filter_string}/>
 
-           return <Cell key={year} year={year} books={existing_books}/>
-             
-        })}
-            
+            {cells.map(year => {
+
+                return <Cell key={year} year={year} books={existing_books} />
+
+            })}
+
         </div>
 
     )
